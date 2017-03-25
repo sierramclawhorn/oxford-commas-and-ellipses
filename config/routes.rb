d8614434f38c9ea7dc681fda7cdcdf36a0e30062
sierramclawhorn <sierramclawhorn@gmail.com>
2017-03-25 13:52:37 -0700

    Create users; Create sessions; Add bcrypt to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
 	resources :posts
 
+	resources :users
+  	get '/logout' => 'sessions#destroy'
+  	get '/login' => 'sessions#new'
+  	post '/login' => 'sessions#create'
+
 	root 'posts#index'
 
 end
